8f580f10016201fe0235fd0e7318da984ba0c886
7hanatos13@gmail.com <7hanatos13@gmail.com@23466cf4-51eb-8628-fd94-4df9d1f33869>
2012-05-03 12:44:00 +0000

    Tạo script CSDL gồm các bảng, chỉ mới nhập liệu cho 3 bảng. Tạo project web application.

---
+++ trunk/PA4/2. Source/WARM/DAO/NguoiDungDAO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAO
+{
+    public class NguoiDungDAO
+    {
+        public static NGUOIDUNG KiemTraThongTinDangNhap(string TenDangNhap, string MatKhau)
+        {            
+            QLNhaHangDataContext db = new QLNhaHangDataContext();
+            foreach (NGUOIDUNG ng in db.NGUOIDUNGs)
+            {
+                if (ng.TenDangNhap == TenDangNhap && ng.MatKhau == MatKhau)
+                    return ng;
+            }
+
+            return null;
+        }
+    }
+}
