254da79fab50746fd193620eb19eb2d0b4595238
7hanatos13@gmail.com <7hanatos13@gmail.com@23466cf4-51eb-8628-fd94-4df9d1f33869>
2012-05-03 12:44:00 +0000

    Tạo script CSDL gồm các bảng, chỉ mới nhập liệu cho 3 bảng. Tạo project web application.

---
+++ PA4/2. Source/WARM/DAO/DanhMucMonAnDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAO
+{
+    public class DanhMucMonAnDAO
+    {
+        public static List<DANHMUCMONAN> LayTatCa()
+        {
+            QLNhaHangDataContext db = new QLNhaHangDataContext();
+            return db.DANHMUCMONANs.ToList();
+        }
+    }
+}
---
+++ PA4/2. Source/WARM/WARM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WARM.Global" Language="C#" %>
