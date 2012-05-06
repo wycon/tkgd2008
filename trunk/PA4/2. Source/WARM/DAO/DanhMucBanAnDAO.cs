d0d635822596f14568ab1c688be3cb36a6061166
7hanatos13@gmail.com <7hanatos13@gmail.com@23466cf4-51eb-8628-fd94-4df9d1f33869>
2012-05-06 02:52:40 +0000

    Cập nhật thêm một số phương thức.

---
+++ trunk/PA4/2. Source/WARM/DAO/DanhMucBanAnDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAO
+{
+    public class DanhMucBanAnDAO
+    {
+        public static List<DANHMUCBANAN> LayTatCa()
+        {
+            QLNhaHangDataContext db = new QLNhaHangDataContext();
+            return db.DANHMUCBANANs.ToList();
+        }
+    }
+}
--- trunk/PA4/2. Source/WARM/DAO/NguoiDungDAO.cs
+++ trunk/PA4/2. Source/WARM/DAO/NguoiDungDAO.cs
@@ -7,6 +7,12 @@ namespace DAO
 {
     public class NguoiDungDAO
     {
+        /// <summary>
+        /// Kiểm tra tên đăng nhập và mật khẩu xem có hợp lệ hay không
+        /// </summary>
+        /// <param name="TenDangNhap"></param>
+        /// <param name="MatKhau"></param>
+        /// <returns>Trả về đối tượng NGUOIDUNG nếu hợp lệ, ngược lại trả về null</returns>
         public static NGUOIDUNG KiemTraThongTinDangNhap(string TenDangNhap, string MatKhau)
         {            
             QLNhaHangDataContext db = new QLNhaHangDataContext();
@@ -17,6 +23,48 @@ namespace DAO
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// Kiểm tra tên đăng nhập có tồn tại chưa
+        /// </summary>
+        /// <param name="TenDangNhap"></param>
+        /// <returns>Trả về true nếu đã tồn tại, ngược lại trả về false</returns>
+        public static bool KiemTraTenDangNhapTonTai(string TenDangNhap)
+        {
+            QLNhaHangDataContext db = new QLNhaHangDataContext();
+            NGUOIDUNG ng = db.NGUOIDUNGs.Single(r => r.TenDangNhap == TenDangNhap);
+            if (ng == null)
+                return false;
+            return true;
+        }
+
+        /// <summary>
+        /// Thêm thông tin người dùng vào CSDL nếu tên đăng nhập chưa tồn tại
+        /// </summary>
+        /// <param name="TenDangNhap"></param>
+        /// <param name="MatKhau"></param>
+        /// <param name="Email"></param>
+        /// <param name="DienThoai"></param>
+        /// <param name="MaLoaiNguoiDung"></param>
+        /// <returns>Trả về đối tượng người dùng nếu thêm thành công, ngược lại trả về null</returns>
+        public static NGUOIDUNG TaoNguoiDung(string TenDangNhap, string MatKhau, string Email, string DienThoai, int MaLoaiNguoiDung)
+        {
+            if (KiemTraTenDangNhapTonTai(TenDangNhap))
+                return null;
+            
+            QLNhaHangDataContext db = new QLNhaHangDataContext();
+            NGUOIDUNG ng = new NGUOIDUNG();
+            ng.TenDangNhap = TenDangNhap;
+            ng.MatKhau = MatKhau;
+            ng.Email = Email;
+            ng.DienThoai = DienThoai;
+            ng.MaLoaiNguoiDung = MaLoaiNguoiDung;
+            db.NGUOIDUNGs.InsertOnSubmit(ng);
+            
+            db.SubmitChanges();
+
+            return ng;
         }
     }
 }
