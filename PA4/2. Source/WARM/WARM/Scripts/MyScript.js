82348f8d537bde055d1eb2ecbc049b130c7411d2
7hanatos13@gmail.com <7hanatos13@gmail.com@23466cf4-51eb-8628-fd94-4df9d1f33869>
2012-05-08 13:44:38 +0000

    Chỉnh sửa dữ liệu: thêm cột đánh giá vào bảng MONAN. Bổ sung các css và script.

---
+++ PA4/2. Source/WARM/WARM/Scripts/MyScript.js
@@ -0,0 +1,25 @@
+﻿function mouseOver(id) {
+    document.getElementById("img").src = document.getElementById(id).src
+}
+function onChange() {
+    //document.getElementById("maTinhTrang").valueOf() = document.getElementById("maTinhTrangGioHang").valueOf()
+}
+function onClick(id) {
+    var e = document.getElementById("maTinhTrangGioHang" + id);
+    var maTinhTrang = e.options[e.selectedIndex].value;
+    frmManageOrder.elements["oderID"].value = id;
+    frmManageOrder.elements["stateID"].value = maTinhTrang;
+    document.forms["frmManageOrder"].submit();
+    //alert(id + "   " + maTinhTrang)
+}
+function datMon(ma, ten, gia) {
+    var e = document.getElementById("phieudatmon");
+    var monan = document.createElement('a');
+    //monan.setAttribute("id", "DDtemp");
+    monan.setAttribute("href", "ChiTietMonAn.aspx?id=" + ma.toString());
+    monan.setAttribute("name", "myanchor");
+    var _text = document.createTextNode(ten);
+    monan.appendChild(_text);
+    monan.appendChild(document.createElement('br'));    
+    e.appendChild(monan);    
+}
