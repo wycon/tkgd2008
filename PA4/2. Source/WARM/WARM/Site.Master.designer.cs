926787b6ee0e10b70936fd6aad4639feb2603bc7
tienvx2008gs@gmail.com <tienvx2008gs@gmail.com@23466cf4-51eb-8628-fd94-4df9d1f33869>
2012-05-20 12:56:43 +0000

    login form.

--- PA4/2. Source/WARM/WARM/Site.Master.designer.cs
+++ PA4/2. Source/WARM/WARM/Site.Master.designer.cs
@@ -31,15 +31,6 @@ namespace WARM {
         protected global::System.Web.UI.WebControls.Image ImageLogo;
         
         /// <summary>
-        /// HeadLoginView control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LoginView HeadLoginView;
-        
-        /// <summary>
         /// NavigationMenu control.
         /// </summary>
         /// <remarks>
